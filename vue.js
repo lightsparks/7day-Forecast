dd94f523c360aec7d5aa8ff8ab8d458ba28240d3
Rinu Yatim <rinuyatim@gmail.com>
2020-09-03 19:36:33 +0200

    Concept build, focus on TODAY

--- vue.js
+++ vue.js
@@ -1,10 +1,25 @@
 const app = new Vue({
     el: '#app',
     data: {
-        message: '',
+        today: {
+            title: 'The weather now',
+            condition: 'Sunny',
+            temperature: '26',
+            degree: "°C",
+        }
     },
-    methods: {
-
-
-    },
-});
+    methods: {},
+    template: `
+        <div id="todayWrap">
+            <div id="todayContainer">
+                <div class="todayImage">
+                    <img alt="sunny" class="today" src="./svg/001-sunny.svg" />
+                </div>
+                <div class="dayText">
+                    <h2 class="today">{{ today.title }}</h2>
+                    <h3 class="today">{{ today.condition }}</h3>
+                </div>
+                <div class="today, temperature">{{ today.temperature }}<span class="temperature">{{ today.degree }}</span></div>
+            </div>
+        </div>`,
+})

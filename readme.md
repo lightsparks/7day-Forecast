e21e832b639bd028a892013ccf4f880f232f9df7
Rinu Yatim <rinuyatim@gmail.com>
2020-09-12 21:51:56 +0200

    files restructured

--- readme.md
+++ readme.md
@@ -1,16 +1,3 @@
 # 7day-forecast
 In this project I try to create a website which will display a 7 day weather forecast.
-To this end I will be using a HTML5 and JavaScript file combo, in which I implement the use of Vue-instances.
-Styling will be done with CSS.
-
-"# 7day-Forecast" 
-
-
-
-Endpoint: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&
-          exclude={part}&appid={YOUR API KEY}
-          
-api_key: 'd1e902b667baccdb37cfbc96ad629f00',
-lat: '52.231589',
-lon: '5.189039',
-url: 'https://api.openweathermap.org/data/2.5/onecall?lat=52.231589&lon=5.189039&exclude=current&appid=d1e902b667baccdb37cfbc96ad629f00',
+To this end I will be using a HTML5 and JavaScript file combo, implementing Vue-instances, and CSS for the styling.
--- vue.js
+++ vue.js
@@ -1,29 +1,42 @@
 const app = new Vue({
     el: '#app',
     data: {
-        today: {
-            title: 'The weather now',
-            condition: 'Sunny',
-            temperature: '26',
-            degree: "°C",
+        api_key: 'd1e902b667baccdb37cfbc96ad629f00',
+        base_url: 'http://api.openweathermap.org/data/2.5/find?q=',
+        currentDate: new Date().toLocaleString(),
+        currentWeather: {},
+        forecastData: {},
+
+        /*        query: '',*/
+    },
+
+    mounted: function () {
+        /*        axios.get(`${this.base_url}weather?q=${this.query}&units=metric&appid=${this.api_key}`)*/
+        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=52.231589&lon=5.189039&exclude=minutely,hourly&units=metric&appid=d1e902b667baccdb37cfbc96ad629f00")
+            .then((response) => {
+                this.currentWeather = {
+                    condition: response.data.current.weather[0].description,
+                    temperatureNow: Math.round(response.data.current.temp),
+                    hiTemp: Math.round(response.data.daily[0].temp.max),
+                    loTemp: Math.round(response.data.daily[0].temp.min),
+                    icon: "./images/" + response.data.current.weather[0].icon + ".png",
+                };
+                this.forecastData = response.data.daily.splice(1, 7);
+                console.log(this.forecastData);
+
+            });
+    },
+
+    methods: {
+        fetchWeather: function () {
+        },
+        datefunction() {
+        },
+        customIcon: function(iconId){
+            return `./images/${iconId}.png`;
         }
     },
-    methods: {},
-    template: `
-        <div id="todayWrap">
-            <div id="todayContainer">
-                <div class="todayImage">
-                    <img alt="sunny" class="today" src="./svg/001-sunny.svg" />
-                </div>
-                <div class="dayText">
-                    <h2 class="today">{{ today.title }}</h2>
-                    <h3 class="today">{{ today.condition }}</h3>
-                </div>
-                <div class="today, temperature">{{ today.temperature }}<span class="temperature">{{ today.degree }}</span></div>
-            </div>
-        </div>`,
-})
+});
 
-vue.component = new vue({
-    data
-})
+
+

c67e4de07df4df0aea48c9e6c2e0b8d27078f0e4
LightSparks <59706991+lightsparks@users.noreply.github.com>
2020-09-14 09:23:54 +0200

    Update vue.js

--- vue.js
+++ vue.js
@@ -10,7 +10,7 @@ const app = new Vue({
 
     mounted: function () {
         /*        axios.get(`${this.base_url}weather?q=${this.query}&units=metric&appid=${this.api_key}`)*/
-        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=52.231589&lon=5.189039&exclude=minutely,hourly&units=metric&appid=d1e902b667baccdb37cfbc96ad629f00")
+        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=52.231589&lon=5.189039&exclude=minutely,hourly&units=metric&lang=nl&appid=d1e902b667baccdb37cfbc96ad629f00")
             .then((response) => {
                 this.currentWeather = {
                     condition: response.data.current.weather[0].description,
